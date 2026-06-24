406d72fc878ed330b66670ec361ae0a2601cf120
chrnz008 <charancuz008@gmail.com>
2026-06-24 12:48:28 +0530

    fix:update instructions and handle secure arg checking with %~ and quote checking

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ this is a simple plugin manager written in batch script for vim.
 originally intended for vim but can be modified to work with any utility
 
 ## dependencies
-1. git 
-2. cmd 
+1. git
+2. cmd
 
 ## usage
 
@@ -14,11 +14,12 @@ plugit i - install
 plugit u - update
 plugit r - remove
 ```
-- the package list is read from packs.txt file 
+- the package list is read from packs.txt file
 - ; (semicolon) can be used to comment lines
 
-## todo 
+## todo
 - [ ] options to remove specific plugins
 - [ ] opt plugins support
 - [ ] support downgrading (tagging)
 - [ ] better messages and interface instead of git messages
+- [ ] Can simplify arg dispatch eg: Instead of nested ifs using labels
--- plugit.bat
+++ plugit.bat
@@ -2,7 +2,10 @@
 
 REM check if git exists
 where git > nul 2>&1
-if %errorlevel%==1 echo git doesnt exist
+if %errorlevel%==1 (
+	echo git doesnt exist
+	exit /B 1
+)
 
 setlocal
 
@@ -14,39 +17,42 @@ if not exist %packdir% ( md %packdir% )
 
 REM check if packs.txt exists
 if not exist packs.txt (
-	echo packs.txt doesnt exist 
+	echo packs.txt doesnt exist
 ) else (
-	if "%1"=="" (
-	   	echo print instructions 
-	)
+	if "%~1"=="" (
+		echo plugit i - install
+		echo plugit u - update
+		echo plugit r - remove
+	) else (
 
-	REM install
-	if %1==i (
-		echo installing
-		for /F "eol=;" %%i in (packs.txt) do (
-			git -C %packdir% clone --depth=1 %%i
+		REM install
+		if "%~1"=="i" (
+			echo installing
+			for /F "eol=;" %%i in (packs.txt) do (
+				git -C %packdir% clone --depth=1 %%i
+			)
 		)
-	)
 
-	REM update
-	if %1==u (
-		echo updating
-		for /F "eol=;" %%I in (packs.txt) do (
-			echo %%~nxI 
-			REM see for /?
-			git -C %packdir%\%%~nxI pull
+		REM update
+		if "%~1"=="u" (
+			echo updating
+			for /F "eol=;" %%I in (packs.txt) do (
+				echo %%~nxI
+				REM see for /?
+				git -C %packdir%\%%~nxI pull
+			)
 		)
-	)
 
-	REM remove all plugins
-	if %1==r (
-		REM TODO remove only specifics
-		REM removing the pack dir is better then looping over subfolders
-		rd /S /Q %packdir%
-		REM note that it just deletes start
-		echo all plugins have been removed
-	)
+		REM remove all plugins
+		if "%~1"=="r" (
+			REM TODO remove only specifics
+			REM removing the pack dir is better then looping over subfolders
+			rd /S /Q %packdir%
+			REM note that it just deletes start
+			echo all plugins have been removed
+		)
 
+	)
 )
 
 endlocal
